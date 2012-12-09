bb68e0847d81fb763fb3fe379e2bae78cb9e5fac
tstrimple@gmail.com <tstrimple@gmail.com>
2012-12-09 11:24:07 -0800

    Added message push to web side.

--- app.js
+++ app.js
@@ -59,14 +59,27 @@ app.post('/tropo/', function(req, res) {
 io.sockets.on('connection', function (socket) {
   socket.emit('log', 'connection');
   socket.on('message', function(message) {
+  	var url = 'https://api.tropo.com/1.0/sessions?action=create&token=TOKEN'
 
   	workers.sendMessageToWorkers(function(worker) {
-	    var tropo = new TropoWebAPI();
-	 
-	    tropo.call(worker.workerId, null, null, null, null, null, "SMS", null, null, null);
-	    tropo.say(message);
-	 
-	    response.end(TropoJSON(tropo));
+
+	var token = '184ffe6388374c4d99084d89e141908d9d0b8508f38c3fadaa',
+		msg = encodeURI('This is a test SMS message from Node.js.'),
+		tropoSessionAPI = 'api.tropo.com',
+		path = '/1.0/sessions?action=create&token=' + token + '&msg=' + message + '&number=' + worker.workerId;
+
+		var tropo = http.createClient(80, tropoSessionAPI);
+		var request = tropo.request('GET', path, {'host': tropoSessionAPI});
+
+		request.end();
+
+		request.on('response', function (response) {
+		  response.setEncoding('utf8');
+		  response.addListener('data', function (chunk) {
+		  sys.log('Sent message. Tropo response code:' + response.statusCode + '. Body: ' + chunk);
+			socket.emit('log', 'The message has been sent to ' + worker.username);
+		  });
+		});         
   	});
   });
 });
