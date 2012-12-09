446e4fedb3273d4fb9fa9b3fade14ba42e6c9c7b
tstrimple@gmail.com <tstrimple@gmail.com>
2012-12-08 20:29:23 -0800

    double stupid

--- app.js
+++ app.js
@@ -2,28 +2,34 @@ var app = require('express')(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
 	port = process.env.PORT || 3000,
-	tropoWebApi = require('tropo-webapi');
+	tropowebapi = require('tropo-webapi');
 
 server.listen(port);
+
+process.on('uncaughtException', function(err) {
+  io.sockets.emit('log', 'zomgwtf? ' + err);
+});
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/tropo/voice.json', function(req, res) {
-	var tropo = new tropoWebApi.TropoWebAPI(); 
+	io.sockets.emit('log', '/tropo/voice.json');
+	var tropo = new tropowebapi.TropoWebAPI(); 
 	tropo.say("Hello, World!");
 
     response.writeHead(200, { 'Content-Type': 'application/json' });
-	response.end(tropoWebApi.TropoJSON(tropo));
+	response.end(tropowebapi.TropoJSON(tropo));
 });
 
 app.get('/tropo/text.json', function(req, res) {
-	var tropo = new tropoWebApi.TropoWebAPI(); 
+	io.sockets.emit('log', '/tropo/text.json');
+	var tropo = new tropowebapi.TropoWebAPI(); 
 	tropo.say("Hello, World!");
 
     response.writeHead(200, { 'Content-Type': 'application/json' });
-	response.end(tropoWebApi.TropoJSON(tropo));
+	response.end(tropowebapi.TropoJSON(tropo));
 });
 
 io.sockets.on('connection', function (socket) {
