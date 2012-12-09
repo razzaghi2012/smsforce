e2bf02f75f1289a8f9368312c6c1ea7adba7a170
tstrimple@gmail.com <tstrimple@gmail.com>
2012-12-08 19:50:26 -0800

    Fixed port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var app = require('express')(), 
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	port = process.env.PORT || 3000;
 
-server.listen(8000);
+server.listen(port);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
