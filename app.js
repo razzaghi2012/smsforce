93e87d9f3ea553189d12332509ca34926df08ad3
tstrimple@gmail.com <tstrimple@gmail.com>
2012-12-08 20:04:09 -0800

    Added tropo placeholder.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var app = require('express')(), 
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	port = process.env.PORT || 3000;
+	port = process.env.PORT || 3000,
+	tropoWebApi = require('tropo-webapi');
 
 server.listen(port);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/tropo/voice.json', function(req, res) {
+	var tropo = new tropowebapi.TropoWebAPI(); 
+	tropo.say("Hello, World!");
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+	response.end(tropowebapi.TropoJSON(tropo));
+});
+
+app.get('/tropo/text.json', function(req, res) {
+	var tropo = new tropowebapi.TropoWebAPI(); 
+	tropo.say("Hello, World!");
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+	response.end(tropowebapi.TropoJSON(tropo));
 });
 
 io.sockets.on('connection', function (socket) {
