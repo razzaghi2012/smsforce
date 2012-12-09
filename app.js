abb288b4b012b5ba0c75c47f550c58fac40f712f
tstrimple@gmail.com <tstrimple@gmail.com>
2012-12-08 21:57:57 -0800

    yes!

--- app.js
+++ app.js
@@ -18,13 +18,7 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-app.post('/tropo/voice.json', function(req, res) {
-	var tropo = new tropowebapi.TropoWebAPI();
-	tropo.say("The voice protocol is not supported at this time.");
-    res.send(tropowebapi.TropoJSON(tropo));
-});
-
-app.post('/tropo/text.json', function(req, res) {
+app.post('/tropo', function(req, res) {
 	io.sockets.emit('log', 'WORKK!!!!' + req.body);
 	var tropo = new tropowebapi.TropoWebAPI();
 	tropo.say("text");
